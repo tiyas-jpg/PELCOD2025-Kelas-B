1825961240050564ae67c3d1f01bd66f74e9b0fc
tiyas-jpg <tiyas528@gmail.com>
2025-09-13 11:43:54 +0700

    250441100110_YukariWahyuNingtiyas_Day1

---
+++ 250441100110-Yukari Wahyu Ningtiyas-day1/pelcod.py
@@ -0,0 +1,29 @@
+# # nama = "Yukari Wahyu Ningtiyas"
+# # nim = 250441100110
+# # semester = 1
+
+# # print("=====BIODATA MAHASISWA=====")
+# # print("Hallo, nama saya:"+ nama + "\n")
+# # print (f"Nim saya: {nim}\n")
+# # print("Saya berkuliah di semester: {}".format(semester))
+
+# a = 10
+# b = 5
+# c = a + b #penjumlahan
+# d = a - b #pengurangan
+# e = a * b #perkalian
+# f = a / b #pembagian
+# g = a % b #sisa bagi
+# print("Hasil Penjumlahan:" , c)
+# print("Hasil Pengurangan:" , d)
+# print("Hasil Perkalian:" , e)
+# print("Hasil Pembagian:" , f)
+# print("Hasil Sisa Bagi:" , g)
+
+nama = input("Masukkan nama Anda:")
+umur = input("Masukkan umur Anda:")
+buah = input("Masukkan buah favorit Anda:")
+
+print("nama saya adalah " + nama)
+print("umur saya adalah " + umur + " tahun")
+print("buah favorit saya adalah " + buah) 
